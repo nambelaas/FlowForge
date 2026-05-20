fd9925bd01c6d88ea49b99e178ba569098aa6050
nambelaas <salmaniseif@gmail.com>
2026-05-20 17:52:53 +0700

    change auth to jwt and adjust middleware api

--- routes/api.php
+++ routes/api.php
@@ -2,21 +2,21 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\WorkflowController;
-use App\Http\Controllers\Api\WorkflowExecutionController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware(['auth:sanctum', 'throttle:60,1'])->group(function () {
+Route::middleware('auth:api')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+});
 
-    // Hanya Editor dan Admin yang bisa membuat atau memperbarui alur kerja
+Route::middleware(['auth:api', 'throttle:60,1'])->group(function () {
     Route::middleware('role:admin,editor')->group(function () {
         Route::post('/workflows', [WorkflowController::class, 'store']);
         Route::put('/workflows/{workflow}', [WorkflowController::class, 'update']);
     });
 
-    // Peran Viewer, Editor, dan Admin semuanya bisa melihat daftar alur kerja
     Route::middleware('role:admin,editor,viewer')->group(function () {
         Route::get('/workflows', [WorkflowController::class, 'index']);
     });
