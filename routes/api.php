ca41b13a65d14f9833514babf2b606d3d717e110
nambelaas <salmaniseif@gmail.com>
2026-05-20 19:31:50 +0700

    add pagination and filter in dashboard

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:api')->group(function () {
-    Route::get('/me', [AuthController::class, 'me']);
-});
-
 Route::middleware(['auth:api', 'throttle:60,1'])->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+
     Route::middleware('role:admin,editor')->group(function () {
         Route::post('/workflows', [WorkflowController::class, 'store']);
         Route::put('/workflows/{workflow}', [WorkflowController::class, 'update']);
